07287ebf7c22ea678b64cd8a405d60cd0d10e535
Adit <aditbhimani007@gmail.com>
2025-03-30 14:34:01 -0400

    Fixed a few bugs for Krish's Inventory screen code, and made it run.

--- src/view/InventoryScreen.java
+++ src/view/InventoryScreen.java
@@ -6,12 +6,18 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import controller.BearController; // added by Adit
+import app.PoCoToApp; // added by Adit
 
 public class InventoryScreen {
     private Stage primaryStage;
+    private BearController controller; // added by Adit
+    private PoCoToApp app; // added by Adit
 
-    public InventoryScreen(Stage primaryStage) {
+    public InventoryScreen(Stage primaryStage, BearController controller, PoCoToApp app) {
         this.primaryStage = primaryStage;
+        this.controller = controller; // added by Adit
+        this.app = app; // added by Adit
         showInventoryScreen();
     }
 
@@ -19,8 +25,8 @@ public class InventoryScreen {
         VBox layout = new VBox(20);
         
         // Load images from assets
-        Image appleImage = new Image(getClass().getResourceAsStream("/assets/apple.png"));
-        Image bananaImage = new Image(getClass().getResourceAsStream("/assets/banana.png"));
+        Image appleImage = new Image("file:assets/apple.png");
+        Image bananaImage = new Image("file:assets/banana.png");
 
         ImageView appleView = new ImageView(appleImage);
         appleView.setFitWidth(100);
@@ -32,12 +38,16 @@ public class InventoryScreen {
 
         // Back button to return to GameplayScreen
         Button backButton = new Button("Back to Game");
-        backButton.setOnAction(e -> new GameplayScreen(primaryStage));
+        backButton.setOnAction(e -> { // modified by Adit to cleanly return to the gameplay screen
+            GameplayScreen gameplay = new GameplayScreen(controller, app);
+            gameplay.start(primaryStage);
+        });
+        
 
         layout.getChildren().addAll(appleView, bananaView, backButton);
         
         Scene scene = new Scene(layout, 800, 600);
-        scene.getStylesheets().add(getClass().getResource("styles.css").toExternalForm());
+        scene.getStylesheets().add(getClass().getResource("/styles.css").toExternalForm()); // modified by Adit to look for styles.css from the root of the classpath
 
         primaryStage.setScene(scene);
         primaryStage.show();
